bdd910ab1d853de98f3c3f39b846ef2f90010071
Antho76 <62425937+Antho76@users.noreply.github.com>
2023-03-14 18:12:26 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,21 @@ const discord = require("discord.js");
 
 module.exports.run = async (client, message, args ) => {
 message.channel.send ("liste des commandes : ");
-message.channel.send("join \n leave \n pause\nping\nplay + musique/URL\nplaynext + musique/URL\nfile\nreapeat + off/musique/file\nresume\nshuffle\nskip\nstop\nvolume + valeur entre 0 et 100");
+message.channel.send(
+   "join \n"+
+   "leave\n"+
+   "pause\n"+
+   "ping\n"+
+   "play + musique/URL\n"+
+   "playnext + musique/URL\n"+
+   "file\n"+
+   "reapeat + off/musique/file\n"+
+   "resume\n"+
+   "shuffle\n"+
+   "skip\n"+
+   "stop\n"+
+   "volume + valeur entre 0 et 100"
+   );
 }
 
 module.exports.help = {
