c3c64006e91fe5472c2006923923825c0b4e44f4
Antho76 <62425937+Antho76@users.noreply.github.com>
2023-03-14 17:57:56 +0100

    Add files via upload

---
+++ commands/Author.js
@@ -0,0 +1,9 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args ) => {
+message.channel.send (" BOT cxréé par Anthonin PAIN ");
+}
+
+module.exports.help = {
+    name: "Author"
+}
