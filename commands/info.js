05cf27714587c4870c34a54366f934f38e51aa26
Antho76 <62425937+Antho76@users.noreply.github.com>
2023-03-14 17:52:50 +0100

    Add files via upload

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,21 @@
 const discord = require("discord.js");
 
 module.exports.run = async (client, message, args ) => {
-
+message.channel.send ("liste des commandes : ");
+message.channel.send("join");
+message.channel.send("leave");
+message.channel.send("pause");
+message.channel.send("ping");
+message.channel.send("play + musique/URL");
+message.channel.send("playnext + musique/URL");
+message.channel.send("file");
+message.channel.send("reapeat + off/musique/file");
+message.channel.send("resume");
+message.channel.send("shuffle");
+message.channel.send("skip");
+message.channel.send("stop");
+message.channel.send("volume + valeur entre 0 et 100");
+message.channel.send("Bot créé par Anthonin PAIN");
 }
 
 module.exports.help = {
